15a92eddd1f3ec8cc665966337ddb5a08dfc53f8
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-01-14 00:18:44 +0300

    HostListener within custom directive listening to events

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[betterHighlight]'
@@ -9,8 +9,16 @@ export class BetterHighlightDirective implements OnInit {
               private elementRef: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  @HostListener('mouseenter') mouseEnter(eventData: Event) {
     // renderer because you may need to render on the server side (Angular Universal)
     this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "palegreen");
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    // renderer because you may need to render on the server side (Angular Universal)
+    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "transparent");
   }
 
 }
